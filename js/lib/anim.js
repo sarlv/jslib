696ea2f9d93a5d2c72ade8fb256db9ae3e430eea
levon <sarlv@dudu.com>
2013-02-18 11:46:59 +0400

    Добавил функция анимации, на базе которой думаю посторить метод Animaition

---
+++ js/lib/anim.js
@@ -0,0 +1,35 @@
+(function() {
+	var anim = $j.namespace('anim');
+
+	$janim = $j.anim = {}
+/*
+
+        barLen = document.defaultView.getComputedStyle(bar) || bar.currentStyle,
+        barLen = parseInt(barLen['width']),
+
+        runLen = document.defaultView.getComputedStyle(run) || run.currentStyle,
+        runLen = parseInt(runLen['width']),
+        init = null,
+
+    Anim = function(e, callback) {
+        var i = 0, param = arguments[0];
+        this.func = function() {
+            if(i < (barLen)) {
+                i += 1;
+                run.style['width'] = runLen + i + 'px';
+            } else {
+                param()
+                init = clearInterval(init);
+            }
+        }
+    }
+    function func() {
+        console.log('Finish!!!');
+    }
+    var anim = new Anim(func);
+
+
+
+*/
+
+}());
